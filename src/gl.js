089df46f3eaa601724bf81218dae20e82798d6fa
Julien Castelain <julien.castelain@liferay.com>
2019-02-10 17:26:13 +0100

    Add WebGL helpers

---
+++ src/gl.js
@@ -0,0 +1,42 @@
+'use strict';
+function createBuffer(gl, target, size, usage) {
+  var buf = gl.createBuffer();
+  gl.bindBuffer(target, buf);
+  gl.bufferData(target, size, usage);
+  return buf;
+}
+
+function createProgram(gl, vert, frag) {
+  var vs = gl.createShader(gl.VERTEX_SHADER);
+  gl.shaderSource(vs, vert);
+  gl.compileShader(vs);
+  // check for errors?
+
+  var fs = gl.createShader(gl.FRAGMENT_SHADER);
+  gl.shaderSource(fs, frag);
+  gl.compileShader(fs);
+
+  var prog = gl.createProgram();
+  gl.attachShader(prog, vs);
+  gl.attachShader(prog, fs);
+  gl.linkProgram(prog);
+
+  return prog;
+}
+
+function createTexture(gl, image) {
+  var tex = gl.createTexture();
+  gl.bindTexture(gl.TEXTURE_2D, tex);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+  gl.texParameteri(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+  gl.bindTexture(gl.TEXTURE_2D, null);
+  return tex;
+}
+
+exports.createBuffer = createBuffer;
+exports.createProgram = createProgram;
+exports.createTexture = createTexture;
+
