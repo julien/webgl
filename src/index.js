e6d97d8ff56c7bb35483355c251b5d1daa4ca7cb
Julien Castelain <julien.castelain@liferay.com>
2019-02-10 17:47:21 +0100

    Update index

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 'use strict';
 var Renderer = require('./renderer');
 var Vec2 = require('./vec2');
+var lib = require('./lib');
 
 var SPRITE_COUNT = 100;
-
+var canvas;
+var renderer;
+var img;
+var mouse;
+var tex;
+var texWidth, texHeight;
 var particles = {
   ax: new Array(SPRITE_COUNT).fill(0),
   ay: new Array(SPRITE_COUNT).fill(0),
@@ -19,6 +25,28 @@ var particles = {
   life: new Array(SPRITE_COUNT).fill(0),
   count: SPRITE_COUNT
 };
-var canvas = document.getElementById('c');
 
-var mouse = new Vec2();
+
+onload = () => {
+  canvas = document.getElementById('c');
+  renderer = new Renderer(canvas);
+
+  img = new Image();
+  img.onload = () => {
+    tex = lib.createTexture(renderer.gl, img);
+    texWidth = img.width;
+    texHeight = img.height;
+    init();
+  };
+  img.src = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAnUlEQVRYR+2XsQ6AIAxEYVD//3PVQcNAYhTKnSlphzKb3uOBUHIyHtk4PwUAbWA/j0tatm1ZqZrwx6PgNxQKAgGw4RUGgRAB/gYzNvwCaM1+tBxNA9rhEoQ/gFmz71n4GAiAMBAGzA2U/3UWROt29HcS1hNL20KvN/B7HWvuBakz8t2SPVsrdk8g/WCpDxlgQNDgWpMG0H7KBYC5gRtfzGAhGEQe7AAAAABJRU5ErkJgggAA';
+
+  mouse = new Vec2(innerWidth * 0.5, innerHeight * 0.5);
+};
+
+function init() {
+
+}
+
+function update() {
+
+}
