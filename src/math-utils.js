f9b05d2e0bccb7124c3326c0925f8d237d673f4f
Julien Castelain <julien.castelain@liferay.com>
2019-02-17 15:53:25 +0100

    Add math utilities

---
+++ src/math-utils.js
@@ -0,0 +1,22 @@
+'use strict';
+
+function lerp(norm, min, max) {
+  return (max - min) * norm + min;
+}
+
+function norm(value, min, max) {
+  return (value - min) / (max - min);
+}
+
+function map(value, sourceMin, sourceMax, destMin, destMax) {
+  return lerp(norm(value, sourceMin, sourceMax), destMin, destMax);
+}
+
+function random(min, max) {
+	return min + Math.random() * (max - min);
+}
+
+exports.lerp = lerp;
+exports.map = map;
+exports.norm = norm;
+exports.random = random;
