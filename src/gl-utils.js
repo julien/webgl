b845379285bb754ba4011cb02373a09807b0a43c
Julien Castelain <julien.castelain@liferay.com>
2020-02-20 21:44:02 +0100

    (Mostly) ES6 everywhere

--- src/gl-utils.js
+++ src/gl-utils.js
@@ -1,41 +1,36 @@
-'use strict';
-function createBuffer(gl, target, size, usage) {
-  var buf = gl.createBuffer();
-  gl.bindBuffer(target, buf);
-  gl.bufferData(target, size, usage);
-  return buf;
+export function createBuffer(gl, target, size, usage) {
+	const buf = gl.createBuffer();
+	gl.bindBuffer(target, buf);
+	gl.bufferData(target, size, usage);
+	return buf;
 }
 
-function createProgram(gl, vert, frag) {
-  var vs = gl.createShader(gl.VERTEX_SHADER);
-  gl.shaderSource(vs, vert);
-  gl.compileShader(vs);
+export function createProgram(gl, vert, frag) {
+	const vs = gl.createShader(gl.VERTEX_SHADER);
+	gl.shaderSource(vs, vert);
+	gl.compileShader(vs);
 
-  var fs = gl.createShader(gl.FRAGMENT_SHADER);
-  gl.shaderSource(fs, frag);
-  gl.compileShader(fs);
+	const fs = gl.createShader(gl.FRAGMENT_SHADER);
+	gl.shaderSource(fs, frag);
+	gl.compileShader(fs);
 
-  var prog = gl.createProgram();
-  gl.attachShader(prog, vs);
-  gl.attachShader(prog, fs);
-  gl.linkProgram(prog);
+	const prog = gl.createProgram();
+	gl.attachShader(prog, vs);
+	gl.attachShader(prog, fs);
+	gl.linkProgram(prog);
 
-  return prog;
+	return prog;
 }
 
-function createTexture(gl, image) {
-  var tex = gl.createTexture();
-  gl.bindTexture(gl.TEXTURE_2D, tex);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
-  gl.bindTexture(gl.TEXTURE_2D, null);
-  return tex;
+export function createTexture(gl, image) {
+	const tex = gl.createTexture();
+	gl.bindTexture(gl.TEXTURE_2D, tex);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+	gl.bindTexture(gl.TEXTURE_2D, null);
+	return tex;
 }
-
-exports.createBuffer = createBuffer;
-exports.createProgram = createProgram;
-exports.createTexture = createTexture;
 
