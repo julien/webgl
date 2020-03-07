68053085d67ebb24d4969ea1a2fb46a4ca8da8ae
Julien Castelain <julien.castelain@liferay.com>
2020-03-07 18:08:38 +0100

    Fix imports

--- src/bubbles.js
+++ src/bubbles.js
@@ -1,7 +1,6 @@
-import Renderer from './renderer.js';
-import vec2 from './vec2.js';
-import {createTexture} from './gl-utils.js';
-import {map, random} from './math-utils.js';
+import Renderer from "./renderer.js";
+import vec2 from "./vec2.js";
+import { createTexture, map, random } from "./utils.js";
 
 const MAX_FLEE_DISTANCE = 400;
 const MIN_FLEE_DISTANCE = 60;
@@ -36,19 +35,16 @@ function spriteArrive(sprites, idx, target) {
 	const force = [0, 0];
 
 	if (idx < SPRITE_COUNT) {
-		const desired = [
-			target[0] - sprites.px[idx],
-			target[1] - sprites.py[idx]
-		];
+		const desired = [target[0] - sprites.px[idx], target[1] - sprites.py[idx]];
 
 		let speed = SPRITE_MAX_SPEED;
-		const dist = vec2.get_mag(desired);
+		const dist = vec2.getMag(desired);
 
 		if (dist < 100) {
 			speed = map(dist, 0, 100, 0, SPRITE_MAX_SPEED);
 		}
 
-		vec2.set_mag(desired, speed);
+		vec2.setMag(desired, speed);
 
 		force[0] = desired[0] - sprites.vx[idx];
 		force[1] = desired[1] - sprites.vy[idx];
@@ -63,15 +59,12 @@ function spriteFlee(sprites, idx, target) {
 	const force = [0, 0];
 
 	if (idx < SPRITE_COUNT) {
-		const desired = [
-			target[0] - sprites.px[idx],
-			target[1] - sprites.py[idx]
-		];
+		const desired = [target[0] - sprites.px[idx], target[1] - sprites.py[idx]];
 
-		const dist = vec2.get_mag(desired);
+		const dist = vec2.getMag(desired);
 
 		if (dist < fleeDistance) {
-			vec2.set_mag(desired, SPRITE_MAX_SPEED);
+			vec2.setMag(desired, SPRITE_MAX_SPEED);
 			desired[0] *= -1;
 			desired[1] *= -1;
 
@@ -146,7 +139,7 @@ function spritesInit() {
 		sprites.px[i] = hw + random(-hw, hw);
 		sprites.py[i] = hh + random(-hh, hh);
 
-		sprites.rgba[i] = Math.random() * 0xFFFFFFFF;
+		sprites.rgba[i] = Math.random() * 0xffffffff;
 
 		const size = 1 + random(0.25, 1.5);
 		sprites.sx[i] = sprites.sy[i] = size;
@@ -196,13 +189,22 @@ function draw() {
 	renderer.cls();
 	for (let i = 0; i < sprites.count; i++) {
 		renderer.col = sprites.rgba[i];
-		renderer.img(texture,
-			-sprites.hw[i], -sprites.hh[i],
-			sprites.dx[i], sprites.dy[i], 0,
-			sprites.px[i], sprites.py[i],
-			sprites.sx[i], sprites.sy[i],
-			sprites.u0[i], sprites.u1[i],
-			sprites.v0[i], sprites.v1[i]);
+		renderer.img(
+			texture,
+			-sprites.hw[i],
+			-sprites.hh[i],
+			sprites.dx[i],
+			sprites.dy[i],
+			0,
+			sprites.px[i],
+			sprites.py[i],
+			sprites.sx[i],
+			sprites.sy[i],
+			sprites.u0[i],
+			sprites.u1[i],
+			sprites.v0[i],
+			sprites.v1[i]
+		);
 	}
 	renderer.flush();
 }
@@ -214,7 +216,7 @@ function loop() {
 }
 
 onload = function() {
-	canvas = document.getElementById('c');
+	canvas = document.getElementById("c");
 	canvas.width = innerWidth;
 	canvas.height = innerHeight;
 
@@ -226,14 +228,14 @@ onload = function() {
 		texture = createTexture(renderer.gl, img);
 		spritesInit();
 	};
-	img.src = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAnUlEQVRYR+2XsQ6AIAxEYVD//3PVQcNAYhTKnSlphzKb3uOBUHIyHtk4PwUAbWA/j0tatm1ZqZrwx6PgNxQKAgGw4RUGgRAB/gYzNvwCaM1+tBxNA9rhEoQ/gFmz71n4GAiAMBAGzA2U/3UWROt29HcS1hNL20KvN/B7HWvuBakz8t2SPVsrdk8g/WCpDxlgQNDgWpMG0H7KBYC5gRtfzGAhGEQe7AAAAABJRU5ErkJgggAA';
+	img.src =
+		"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAnUlEQVRYR+2XsQ6AIAxEYVD//3PVQcNAYhTKnSlphzKb3uOBUHIyHtk4PwUAbWA/j0tatm1ZqZrwx6PgNxQKAgGw4RUGgRAB/gYzNvwCaM1+tBxNA9rhEoQ/gFmz71n4GAiAMBAGzA2U/3UWROt29HcS1hNL20KvN/B7HWvuBakz8t2SPVsrdk8g/WCpDxlgQNDgWpMG0H7KBYC5gRtfzGAhGEQe7AAAAABJRU5ErkJgggAA";
 
 	mouse = [canvas.width * 0.5, canvas.height * 0.5];
 
-	document.body.addEventListener('mousemove', onMouseMove, false);
-	document.body.addEventListener('touchmove', onTouchMove, false);
-	document.addEventListener('mousedown', onMouseDown, false);
-	document.addEventListener('mousewheel', onMouseWheel, false);
-	canvas.addEventListener('contextmenu', e => e.preventDefault());
+	document.body.addEventListener("mousemove", onMouseMove, false);
+	document.body.addEventListener("touchmove", onTouchMove, false);
+	document.addEventListener("mousedown", onMouseDown, false);
+	document.addEventListener("mousewheel", onMouseWheel, false);
+	canvas.addEventListener("contextmenu", e => e.preventDefault());
 };
-
