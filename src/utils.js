3f235a32c9a316c9eb8e1e14a29afce7f553e69a
Julien Castelain <julien.castelain@liferay.com>
2020-03-07 18:07:29 +0100

    Rename to utils

--- src/utils.js
+++ src/utils.js
@@ -34,3 +34,18 @@ export function createTexture(gl, image) {
 	return tex;
 }
 
+export function lerp(norm, min, max) {
+	return (max - min) * norm + min;
+}
+
+export function norm(value, min, max) {
+	return (value - min) / (max - min);
+}
+
+export function map(value, sourceMin, sourceMax, destMin, destMax) {
+	return lerp(norm(value, sourceMin, sourceMax), destMin, destMax);
+}
+
+export function random(min, max) {
+	return min + Math.random() * (max - min);
+}
