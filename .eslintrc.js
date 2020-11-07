3555992c3506acf837ed2c4dc3868dab16713531
Julien Castelain <julien.castelain@liferay.com>
2020-11-07 20:05:12 +0100

    Update eslint settings
    T.I.L.: eslint and prettier don't always play nicely

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,17 @@
 module.exports = {
-	"env": {
-		"browser": true,
-		"es6": true
+	env: {
+		browser: true,
+		es6: true,
 	},
-	"extends": "eslint:recommended",
-	"parserOptions": {
-		"ecmaVersion": 6,
-		"sourceType": "module"
+	extends: "eslint:recommended",
+	parserOptions: {
+		ecmaVersion: 6,
+		sourceType: "module",
 	},
-	"rules": {
-		"indent": [
-			"error",
-			"tab"
-		],
-		"linebreak-style": [
-			"error",
-			"unix"
-		],
-		"quotes": [
-			"error",
-			"single"
-		],
-		"semi": [
-			"error",
-			"always"
-		]
-	}
+	rules: {
+		indent: ["error", "tab"],
+		"linebreak-style": ["error", "unix"],
+		quotes: ["error", "double"],
+		semi: ["error", "always"],
+	},
 };
